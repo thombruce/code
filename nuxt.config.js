5c033664186196e70988662f3c4610033d58ee79
Thom Bruce <thom@thombruce.com>
2021-06-07 03:14:42 +0100

    Migrate taxonomy handling to nuxt-taxonomies

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@thombruce/tnt'
+    '@thombruce/tnt',
+    '@thombruce/nuxt-taxonomies'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
