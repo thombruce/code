caf760cebf381cdb8bc12ef31c8d095026dd47e8
Thom Bruce <thom@thombruce.com>
2021-06-20 20:34:36 +0100

    Update TNT

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,31 @@
+import tnt from './tnt.config.js'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  // We could... create and import a tnt.json file and utilise its values here... and elsewhere throughout the file.
+  env: {
+    siteTitle: tnt.title,
+    siteDescription: tnt.description,
+    baseUrl: (process.env.NODE_ENV === 'production' ? tnt.url : 'http://localhost:3000'),
+    siteImg: tnt.image
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Jameater',
+    title: tnt.title,
+    titleTemplate: `%s | ${tnt.title}`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'My personal Jamstack blog.' },
-      { hid: 'og:site_name', property: 'og:site_name', content: 'Jameater' },
-      { hid: 'og:title', property: 'og:title', content: 'Jameater' },
+      { hid: 'description', name: 'description', content: tnt.description },
+      { hid: 'og:site_name', property: 'og:site_name', content: tnt.title },
+      { hid: 'og:title', property: 'og:title', content: tnt.title },
+      { hid: 'og:description', property: 'og:description', content: tnt.description },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:url', property: 'og:url', content: 'https://jam.thombruce.com/' },
-      { hid: 'og:image', property: 'og:image', content: 'https://jam.thombruce.com/strawberries.jpg' }
+      { hid: 'og:url', property: 'og:url', content: `${tnt.url}/` },
+      { hid: 'og:image', property: 'og:image', content: `${tnt.url}${tnt.image}` }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
---
+++ tnt.config.js
@@ -0,0 +1,6 @@
+export default {
+  title: 'Jameater',
+  description: 'My personal Jamstack blog.',
+  url: 'https://jam.thombruce.com',
+  image: '/strawberries.jpg'
+}
