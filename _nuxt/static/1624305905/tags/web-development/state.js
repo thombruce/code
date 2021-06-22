9adbca99ce23abde7fb7956e4c208d6b12857ec7
thombruce <thombruce@users.noreply.github.com>
2021-06-22 01:43:32 +0000

    deploy: 56de763c801a2dd10f4e8f9248e86814ab980b59

--- _nuxt/static/1624305905/tags/web-development/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624305905",layout:"default",error:a,serverRendered:b,routePath:"\u002Ftags\u002Fweb-development",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f6f491e2"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
