0d9826d1354b5c4e58b8772b5190733493e7381a
Thom Bruce <thom@thombruce.com>
2021-08-07 17:15:19 +0100

    @thombruce/tnt compatibility fixes

--- components/tnt/layouts/Header.vue
+++ components/tnt/layouts/Header.vue
@@ -2,13 +2,11 @@
 header
   span.py-24
     NuxtLink(to='/') UNDEFINED
-  div
+  .navbar
     nav
-      ul
-        li(v-for='(item, i) in items' :key='i')
-          NuxtLink(:to='item.to')
-            span {{ item.title }}
-    TntUIDarkmodeToggle
+      NuxtLink(v-for='(item, i) in items' :key='i' :to='item.to')
+        span {{ item.title }}
+    TntUIThemeToggle
 </template>
 
 <script>
@@ -34,21 +32,10 @@ export default {
 header {
   @apply flex flex-col justify-center py-5 px-4;
   & > span:first-of-type {
-    @apply block w-min m-auto text-5xl sm:text-7xl md:text-8xl text-primary font-poster;
+    @apply block w-min m-auto text-5xl sm:text-7xl md:text-8xl text-base-content font-poster;
   }
   & > div {
     @apply flex justify-center;
-    & > nav {
-      & ul {
-        @apply flex;
-        & li a {
-          @apply block px-3 md:px-4;
-        }
-      }
-    }
-    & > button, & > a, & > span:not(:first-of-type) {
-      @apply block px-4;
-    }
   }
 }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,8 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   theme: {
-    // To override any of the default colors
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.coolGray,
-      red: colors.red,
-      yellow: colors.amber,
-      green: colors.emerald,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      purple: colors.violet,
-      pink: colors.pink
-    },
     // To extend the color palette (though this can also be done by adding additional colors above)
     extend: {
-      colors: {
-        primary: { ...colors.violet, ...{ DEFAULT: colors.violet['500'] } },
-        dark: { ...colors.coolGray, ...{ DEFAULT: colors.coolGray['900'] } },
-        light: { ...colors.emerald, ...{ DEFAULT: colors.emerald['50'] } }
-      },
       // To add fonts
       fontFamily: {
         poster: ['Ubuntu Mono']
