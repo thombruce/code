1434cb890b959c86850d1ae4232c2cf15462305a
Thom Bruce <thom@thombruce.com>
2021-08-09 01:39:55 +0100

    Add my works

---
+++ pages/work/index.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+article
+  TntBlogList(:articles='articles')
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const articles = await $content('work', { deep: true })
+      .where({ draft: { $ne: true } })
+      .sortBy('date', 'desc')
+      .fetch()
+      .catch(() => {})
+
+    return { articles }
+  }
+}
+</script>
