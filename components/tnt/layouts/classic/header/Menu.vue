f9f386de91cc374eaa8689e6ac0ccc1ea30257ee
Thom Bruce <thom@thombruce.com>
2021-08-17 19:10:54 +0100

    yarn upgrade && TNT updates

---
+++ components/tnt/layouts/classic/header/Menu.vue
@@ -0,0 +1,36 @@
+<template lang='pug'>
+.navbar
+  nav
+    NuxtLink.btn.btn-ghost(v-for='(item, i) in items' :key='i' :to='item.to')
+      span {{ item.title }}
+  TntUIThemeToggle
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Blog',
+          to: '/blog'
+        },
+        {
+          title: 'My Work',
+          to: '/work'
+        },
+        {
+          title: 'Credits',
+          to: '/credits'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='postcss' scoped>
+.navbar {
+  @apply flex justify-center;
+}
+</style>
---
+++ components/tnt/layouts/classic/header/Title.vue
@@ -0,0 +1,10 @@
+<template lang='pug'>
+span.py-24
+  NuxtLink(to='/') CODE
+</template>
+
+<style lang='postcss' scoped>
+span {
+  @apply block w-min m-auto text-5xl sm:text-7xl md:text-8xl text-base-content font-poster;
+}
+</style>
