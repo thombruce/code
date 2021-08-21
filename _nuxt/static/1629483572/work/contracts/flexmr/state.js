ef0397754a33a7d982cb73248356ebd373eb757f
thombruce <thombruce@users.noreply.github.com>
2021-08-21 14:57:24 +0000

    deploy: 154f981d8c7f76e0639d948806cf80acaf5743a9

--- _nuxt/static/1629483572/work/contracts/flexmr/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629483572",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fcontracts\u002Fflexmr",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4365cf4f"}}}}(null));
---
+++ _nuxt/static/1629557830/blog/what-is-the-jamstack/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629557830",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwhat-is-the-jamstack",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"365ae799"}}}}(null));
