3dcc1cb21a5c3d65d1d969893b5421842fa6371b
thombruce <thombruce@users.noreply.github.com>
2021-08-28 01:02:30 +0000

    deploy: f1ce23ddc63a6effa23abd7886b3bbb9664b92f2

--- _nuxt/static/1630069402/work/websites/popcorn/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630069402",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fwebsites\u002Fpopcorn",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1292dd5a"}}}}(null));
---
+++ _nuxt/static/1630112536/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630112536",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3c1a7e5c"}},_img:{"/_ipx/code.jpg?s=200_200":"\u002F_nuxt\u002Fimage\u002Fa0da87.jpg"}}}(null,"\u002F"));
