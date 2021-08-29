750ca6a3bf284b00ae0905379561255b58c0c7ed
thombruce <thombruce@users.noreply.github.com>
2021-08-29 02:02:32 +0000

    deploy: 3c8e49b5732b71760a9462a89eaef067710e4e1b

---
+++ _nuxt/static/1630202538/work/websites/free-as-in-beer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630202538",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fwebsites\u002Ffree-as-in-beer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"612209bb"}}}}(null));
