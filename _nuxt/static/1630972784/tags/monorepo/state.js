c245451bfa74a37dfebf599f667ec84efb7dbf64
thombruce <thombruce@users.noreply.github.com>
2021-09-06 23:59:58 +0000

    deploy: 0e26b98d0bfa9d4e420639f6fe050bf77b2c016b

---
+++ _nuxt/static/1630972784/tags/monorepo/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630972784",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002Fmonorepo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c27dc889"}}}}(null));
