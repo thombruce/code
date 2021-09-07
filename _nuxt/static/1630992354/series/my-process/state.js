bfd4460405f48b7a557e4b05a978b05b923342ff
thombruce <thombruce@users.noreply.github.com>
2021-09-07 17:40:56 +0000

    deploy: a26e62ae17e92bb596bfccd01a296f69312996ed

--- _nuxt/static/1630992354/series/my-process/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630992354",layout:"default",error:a,serverRendered:true,routePath:"\u002Fseries\u002Fmy-process",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"46e5a7b0"}}}}(null));
